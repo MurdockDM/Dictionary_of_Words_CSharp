3252a40e6f84502b21adccbc89cef90656839b95
Dustin Murdock <dmurdock2@gmail.com>
2020-07-15 15:20:19 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary_of_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+        Create a dictionary with key value pairs to
+        represent words (key) and its definition (value)
+    */
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+
+            // Add several more words and their definitions
+            wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+            wordsAndDefinitions.Add("Sojourn", "a temporary stay");
+            wordsAndDefinitions.Add("Cetology", "the branch of zoology dealing with whales and dolphins");
+
+            /*
+                Use square brackets to get the definition of two of the
+                words and then output them to the console
+            */
+            Console.WriteLine(wordsAndDefinitions["Sojourn"]);
+            Console.WriteLine(wordsAndDefinitions["Cetology"]);
+            /*
+                Below, loop over the wordsAndDefinitions dictionary to get the following output:
+                    The definition of (WORD) is (DEFINITION)
+                    The definition of (WORD) is (DEFINITION)
+                    The definition of (WORD) is (DEFINITION)
+            */
+            foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+            {
+                Console.WriteLine($"The definition of {word.Key} is {word.Value} ");
+            }
+        }
+    }
+}
